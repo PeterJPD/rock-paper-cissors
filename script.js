43ead3b465b5933a737a6bb9539b47741a493fa3
Pedro Drago <pedrodrago.profissional@gmail.com>
2022-06-06 11:46:12 -0300

    Add game function and change variables names

--- script.js
+++ script.js
@@ -1,49 +1,81 @@
+let winner = ""
+let playerCounter = 0
+let computerCounter = 0
+
+
+
 function computerPlay(){
-    let machine=Math.floor(Math.random()*3)
+    machine=Math.floor(Math.random()*3)
     return machine
-} 
-
-let computerSelection = computerPlay();
-let playerSelectionGeneral= prompt("Write your choice");
-let playerSelectionLower = playerSelectionGeneral.toLowerCase()
-//0 = rock --- 1 = paper --- 2 = scissors
-
+};
 function playRound(playerSelectionLower, computerSelection){
     if (playerSelectionLower==="rock"){
         if (computerSelection===0){
-            return ("Computer played rock. its a tie.");
+            winner = "tie";
+            return ("Computer played rock. its a tie.")
         };
         if (computerSelection===1){
+            winner = "computer";
             return ("Computer played paper. you lost.")
         };
         if (computerSelection===2){
+            winner= "player";
             return ("Computer played scissors. You won")
         };
     }else if (playerSelectionLower==="paper"){
         if (computerSelection===0){
-            return ("Computer played rock. You won.");
+            winner= "player";
+            return ("Computer played rock. You won.")
         };
         if (computerSelection===1){
+            winner = "tie";
             return ("Computer played paper. Its a tie.")
         };
         if (computerSelection===2){
+            winner = "computer";
+
             return ("Computer played scissors. You lost")
         };
     }else if (playerSelectionLower==="scissors"){
         if (computerSelection===0){
-            return ("Computer played rock. You lost.");
+            winner = "computer";
+            return ("Computer played rock. You lost.")
         };
         if (computerSelection===1){
+            winner= "player";
             return ("Computer played paper. You won.")
         };
         if (computerSelection===2){
+            winner = "tie";
             return ("Computer played scissors. Its a tie.")
         };
     };
 };
-
 function game(){
-    for(let i=0; i<5; i++){
-        return playRound(playerSelectionLower, computerSelection)
-    }
+    for (i = 0; i < 5; i++){
+        computerSelection = computerPlay();
+        playerSelectionGeneral= prompt("Write your choice");
+        playerSelectionLower = playerSelectionGeneral.toLowerCase();
+        result = playRound(playerSelectionLower, computerSelection)
+
+        if (winner==="tie"){
+            playerCounter+=1
+            computerCounter+=1
+
+        };if(winner==="player"){
+            playerCounter+=1
+
+        };if(winner==="computer"){
+            computerCounter+=1
+
+        };
+        console.log(result)
+
+        };console.log(`The final score was ${playerCounter} points for you and ${computerCounter} points for the machine.`);
 }
+
+
+
+
+
+console.log(game())
