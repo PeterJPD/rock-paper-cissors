7dec98e913582df4f33b875dcc9abcc4c38b3ca9
Drago <pedrodrago.profissional@gmail.com>
2022-06-20 15:11:42 -0300

    Add the total score and the winner messages to event listener

--- script.js
+++ script.js
@@ -12,51 +12,79 @@ function playRound(playerSelection, computerSelection){
     if (playerSelection==="rock"){
         if (computerSelection===0){
             winner = "tie";
+            playerCounter+=0
+            computerCounter+=0
             return ("Computer played rock. its a tie.")
         };
         if (computerSelection===1){
             winner = "computer";
-            return ("Computer played paper. you lost.")
+            playerCounter+=0
+            computerCounter+=1
+            return ("Computer played paper. you lost this round.")
         };
         if (computerSelection===2){
             winner= "player";
-            return ("Computer played scissors. You won")
+            playerCounter+=1
+            computerCounter+=0
+            return ("Computer played scissors. You won this round.")
         };
     }else if (playerSelection==="paper"){
         if (computerSelection===0){
             winner= "player";
-            return ("Computer played rock. You won.")
+            playerCounter+=1
+            computerCounter+=0
+            return ("Computer played rock. You won this round.")
         };
         if (computerSelection===1){
             winner = "tie";
+            playerCounter+=0
+            computerCounter+=0
             return ("Computer played paper. Its a tie.")
         };
         if (computerSelection===2){
             winner = "computer";
-
-            return ("Computer played scissors. You lost")
+            playerCounter+=0
+            computerCounter+=1
+            return ("Computer played scissors. You lost this round.")
         };
     }else if (playerSelection==="scissors"){
         if (computerSelection===0){
             winner = "computer";
-            return ("Computer played rock. You lost.")
+            playerCounter+=0
+            computerCounter+=1
+            return ("Computer played rock. You lost this round.")
         };
         if (computerSelection===1){
             winner= "player";
-            return ("Computer played paper. You won.")
+            playerCounter+=1
+            computerCounter+=0
+            return ("Computer played paper. You won this round.")
         };
         if (computerSelection===2){
             winner = "tie";
+            playerCounter+=0
+            computerCounter+=0
             return ("Computer played scissors. Its a tie.")
         };
     };
 };
 
 let buttons = document.querySelectorAll('button');
+let results = document.querySelector('#results');
+let finalResult= document.querySelector('#final-result')
+
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         playerSelection = button.id;
-        (playRound(playerSelection, computerPlay()))
+        let round = playRound(playerSelection, computerPlay())
+        results.textContent = `${round} The machine score is ${computerCounter} and your score is ${playerCounter}`;
+        if (playerCounter>=5){
+            finalResult.textContent = "You Won!"
+        }
+        else if(computerCounter>=5){
+            finalResult.textContent = "You loose."
+        }
+        
     })
 })
